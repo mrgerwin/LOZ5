059e8b767a7cb76c6acb616d1228c55ef54f318f
mrgerwin <jgerwin@otsegoknights.org>
2020-07-21 13:24:00 -0400

    Add files via upload

---
+++ game1.3.py
@@ -0,0 +1,42 @@
+from pygame_functions3 import *
+
+screenSize(1024,768)
+setBackgroundColour('grey')
+setAutoUpdate(False)
+link = Player()
+octorok = Enemy("Octorok.png", 4, 2)
+showSprite(link)
+showSprite(octorok)
+moveSprite(octorok, 200, 200)
+
+nextFrame = clock()
+frame = 0
+
+while True:
+    if clock() >nextFrame:
+        frame= (frame + 1)%2
+        nextFrame += 80
+        pause(10)
+        
+        if keyPressed("down"):
+            
+            link.orientation =0
+            link.move(frame)
+        if keyPressed("up"):
+            link.orientation =1
+            link.move(frame)
+        if keyPressed("right"):
+            link.orientation =2
+            link.move(frame)
+        if keyPressed("left"):
+            link.orientation =3
+            link.move(frame)
+        if keyPressed("space"):
+            changeSpriteImage(link, link.orientation + 8)
+        if keyPressed("h"):
+            changeSpriteImage(link, frame+12)
+        
+        
+        updateDisplay()
+
+endWait()
